5c7ec4036c1fcc1348eacfbfc06415ebf10a0930
tsteele <tsteele@madisoncollege.edu>
2018-06-04 22:35:46 -0500

    Initial commit.

---
+++ src/app/app.component.ts
@@ -0,0 +1,53 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'first-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'first-app';
+
+  // quizzes = [
+  //   "Quiz 1"
+  //   , "Quiz 2"
+  //   , "Quiz 3"
+  // ];
+
+  public quizzes = [
+    {
+      name: "Quiz 1"
+      , showDelete: false
+    }
+    , {
+      name: "Quiz 2"
+      , showDelete: false
+    }
+    , {
+      name: "Quiz 3"
+      , showDelete: false
+    }
+  ];
+
+  public addFunnyQuiz() {
+    this.quizzes.push({
+      name: "Funny Quiz"
+      , showDelete: false 
+    });
+    //console.log(this.newQuizName);
+  }
+
+  public newQuizName; // = "foo";
+
+  public addQuiz() {
+    this.quizzes.push({ 
+      name: this.newQuizName 
+      , showDelete: true
+    });
+    this.newQuizName = "";
+  }
+
+  public deleteQuiz(quizToDelete) {
+    this.quizzes = this.quizzes.filter(x => x !== quizToDelete);
+  }
+}
