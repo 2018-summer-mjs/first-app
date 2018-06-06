abebe50877eef67661a60b453fdb21d8fc588e1d
tsteelematc <tsteele@madisoncollege.edu>
2018-06-06 15:20:17 -0500

    It works ! ! !

--- src/app/choose-question-type-component/choose-question-type-component.component.ts
+++ src/app/choose-question-type-component/choose-question-type-component.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { QuestionTypeService } from '../question-type.service';
 
 @Component({
   selector: 'app-choose-question-type-component',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChooseQuestionTypeComponentComponent implements OnInit {
 
-  constructor() { }
+  constructor(private qtSvc: QuestionTypeService) { }
 
+  // View Model properties...
+  public questionTypes: string[];
+
+  // Life cycle hook.
   ngOnInit() {
+
+    this.questionTypes = this.qtSvc.getAvailableQuestionTypes();
+    
   }
 
 }
