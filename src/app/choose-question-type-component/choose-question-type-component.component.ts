28bcc44d4d39d5202d771d52dd11803d836c1454
tsteelematc <tsteele@madisoncollege.edu>
2018-06-06 14:48:00 -0500

    Generated component.
    ng generate component choose-question-type-component

---
+++ src/app/choose-question-type-component/choose-question-type-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-choose-question-type-component',
+  templateUrl: './choose-question-type-component.component.html',
+  styleUrls: ['./choose-question-type-component.component.css']
+})
+export class ChooseQuestionTypeComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
