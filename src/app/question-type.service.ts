1e7345dd9e72be71bfa380a009891a2725296d41
brent presley <presley.brent@gmail.com>
2018-06-07 11:20:37 -0500

    import http client for web api access

--- src/app/question-type.service.ts
+++ src/app/question-type.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
+// import { HttpClient } from 'selenium-webdriver/http';
+import { HttpClient } from '@angular/common/http'
 
 @Injectable({
   providedIn: 'root'
 })
 export class QuestionTypeService {
 
-  constructor() { }
+  constructor(private builInAngularHTTPClient: HttpClient) { }
 
   public getAvailableQuestionTypes() {
     return [
