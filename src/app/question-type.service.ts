47e0301ff7acaa55d6fa3bfd8f1a2dc9c1e8ef10
NWTC Student <student@nwtc-web.com>
2018-06-07 11:23:27 -0500

    Added - HTTP (AJAX using Observables)
    Observables - a stream based simple HTPT client api

--- src/app/question-type.service.ts
+++ src/app/question-type.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class QuestionTypeService {
 
-  constructor() { }
+  constructor(private builtInAngularHttpClient: HttpClient) { }
 
   public getAvailableQuestionsTypes() {
     return [
