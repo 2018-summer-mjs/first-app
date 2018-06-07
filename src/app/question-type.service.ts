6341f1e9f0b8a75993e68947ddbaf5090124d386
Kari <kkdaggs@madisoncollege.edu>
2018-06-07 11:19:04 -0500

    starting webAPI Demo - added HttpClient dependency
    Had to import the HttpClientModule as well.

--- src/app/question-type.service.ts
+++ src/app/question-type.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class QuestionTypeService {
 
-  constructor() { }
+  constructor(private builtInAngularHttpClient: HttpClient) { }
 
   public getAvailableQuestionTypes() {
     return [
