2f2b493b1eee7f8fc9e39ff88c10985ae908db73
tsteele <tsteele@madisoncollege.edu>
2018-06-06 22:56:08 -0500

    HttpClient...
    Better than last semester. But Observables : - O
    
    Hmm...

--- src/app/choose-question-type-component/choose-question-type-component.component.ts
+++ src/app/choose-question-type-component/choose-question-type-component.component.ts
@@ -18,12 +18,15 @@ export class ChooseQuestionTypeComponentComponent implements OnInit {
 
   // View Model properties...
   public questionTypes: QuestionTypeDisplay[];
-
+  public errorLoadingQuestionTypes = false;
+  
   // Life cycle hook.
   ngOnInit() {
 
-    this.questionTypes = this.qtSvc.getAvailableQuestionTypes().map(x => ({ name: x, checked: false }));
-    
+    this.qtSvc.getAvailableQuestionTypes()
+      .subscribe(data => this.questionTypes = 
+        data.map(x => ({ name: x, checked: false }))
+        , error => this.errorLoadingQuestionTypes = true
+      );
   }
-
 }
--- src/app/question-type.service.ts
+++ src/app/question-type.service.ts
@@ -1,18 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class QuestionTypeService {
 
-  constructor() { }
+  constructor(private angularBuiltInHttpService: HttpClient) { }
 
   public getAvailableQuestionTypes() {
-    return [
-      "Multiple Choice"
-      , "True/False"
-      , "Essay"
-      , "Short Answer"
-    ];
+    // return [
+    //   "Multiple Choice"
+    //   , "True/False"
+    //   , "Essay"
+    //   , "Short Answer"
+    // ];
+    return this.angularBuiltInHttpService.get<string[]>("https://modern-js.azurewebsites.net/api/GetQuestionTypes?name=foo");
   }
 }
