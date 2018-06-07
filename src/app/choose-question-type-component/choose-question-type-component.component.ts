ee20861b04c8452c827ff165952c645208dfc2fe
brent presley <presley.brent@gmail.com>
2018-06-07 14:12:35 -0500

    promises and await async's

--- src/app/choose-question-type-component/choose-question-type-component.component.ts
+++ src/app/choose-question-type-component/choose-question-type-component.component.ts
@@ -30,9 +30,57 @@ export class ChooseQuestionTypeComponentComponent implements OnInit {
 
     this.qtSvc.getAvailableQuestionTypes().subscribe(
       data => this.questionTypes = data.map(x => ({ name: x, checked: false }))
-      , error => {this.errorLoadingQuestionTypes = true;
-        console.log(error);}
+      , error => {
+        this.errorLoadingQuestionTypes = true;
+        console.log(error);
+      }
     );
+
+    //test the failure
+    // let p = this.qtSvc.getAvailableQuestionTypeCount(false);
+
+    //the success
+    let p = this.qtSvc.getAvailableQuestionTypeCount(true);
+
+    //when the promise is resolved, run the then... if error, run catch.. 
+    p.then(data => console.log(data))
+      .catch(e => console.log(e));
+
+    console.log("here");
+
+
+    this.getCountAsync();
   }
 
+  //we will await the asyncronous calls.  When the promise resolves, will assign this to n.
+  private async getCountAsync() {
+
+    try {
+      // let n = await this.qtSvc.getAvailableQuestionTypeCount(true);  //function stops here until the promise resolves successfully
+      // let n2 = await this.qtSvc.getAvailableQuestionTypeCount(false);  //function stops here until the promise resolves successfully
+      // console.log(n);
+      // console.log("here2");
+
+
+      let n = this.qtSvc.getAvailableQuestionTypeCount(false);  //function stops here until the promise resolves successfully
+      let n2 = this.qtSvc.getAvailableQuestionTypeCount(true);  //function stops here until the promise resolves successfully
+      console.log(n);
+      console.log("here2");
+
+
+      //try a race condition, only take the first one
+      // let results = await Promise.race([n, n2]);
+      // console.log(results);
+
+      //returns an array.   all of them must resovle properly.  
+      let results = await Promise.all([n, n2]);
+      console.log(results[0]);
+      console.log(results[1]);
+      console.log("here2");
+    }
+
+    catch (cat) {
+      console.log(cat);
+    }
+  }
 }
--- src/app/question-type.service.ts
+++ src/app/question-type.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 // import { HttpClient } from 'selenium-webdriver/http';
 import { HttpClient } from '@angular/common/http'
+import { promise } from 'protractor';
 
 @Injectable({
   providedIn: 'root'
@@ -8,15 +9,15 @@ import { HttpClient } from '@angular/common/http'
 export class QuestionTypeService {
 
   constructor(private builtInAngularHttpClient: HttpClient) { }
+  //promise is done in thebackground.  
+  public getAvailableQuestionTypes() {
+    return this.builtInAngularHttpClient
+      .get<string[]>("https://modern-js.azurewebsites.net/api/GetQuestionTypes?name=magic%20Quiz");
 
-   public getAvailableQuestionTypes() {
-     return this.builtInAngularHttpClient
-       .get<string[]>("https://modern-js.azurewebsites.net/api/GetQuestionTypes?name=magic%20Quiz");
 
-
-// public getAvailableQuestionTypes() {
-//   return this.builtInAngularHttpClient
-//     .get<string[]>("https://modern-js.azurewebsites.net/api/GetQuestionTypes?name1=magic%20Quiz");
+    // public getAvailableQuestionTypes() {
+    //   return this.builtInAngularHttpClient
+    //     .get<string[]>("https://modern-js.azurewebsites.net/api/GetQuestionTypes?name1=magic%20Quiz");
 
 
     // return [
@@ -25,5 +26,15 @@ export class QuestionTypeService {
     //   , "Essay"
     //   , "Short Answer"
     // ];
+  }
+
+  public getAvailableQuestionTypeCount(succeed: boolean): Promise<number> {
+    let p = new Promise<number>(
+      (resolve, reject) => {
+        succeed ? resolve(42) : reject("failed")
+      }
+    );
+
+    return p; //return the promise of a number...
   }
 }
